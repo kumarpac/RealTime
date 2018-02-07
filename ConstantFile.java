8eb07525c2b032d3b13fa65af3a6df4c17be1363
kumarpac <dslkumarpawan@gmail.com>
2018-02-07 16:01:32 +0000

    Update ConstantFile.java

--- ConstantFile.java
+++ ConstantFile.java
@@ -2,7 +2,7 @@ package hor.us.constant;
 
 public class ConstantFile {
 
-	public final static String topicName= "svc-horizon";
+	public final static String topicName= "svc-ads";
 	public final static String fileName = "/home/hadoop/test.txt";
 	
 }
